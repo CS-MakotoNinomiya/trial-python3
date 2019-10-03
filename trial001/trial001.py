79edf200beb60b42e7f15bbe269f21f58750e6c7
Ninomiya <m.ninomiya@central-soft.co.jp>
2019-10-03 17:23:41 +0900

    ファイル出力時に日時を書きだす処理を追加

--- trial001/trial001.py
+++ trial001/trial001.py
@@ -1,7 +1,17 @@
 import os
+import datetime
 import pathlib
 
 base = os.path.dirname(os.path.abspath(__file__))
 name = os.path.normpath(os.path.join(base, "./conf/sample.txt"))
 
 print(pathlib.Path(name).exists())
+
+try:
+    dt_now = datetime.datetime.now()
+    file = open(name, 'a')
+    file.write("{0:%Y-%m-%d %H:%M:%S}".format(dt_now) + " write to file.\n")
+except Exception as e:
+    print(e)
+finally:
+    file.close()
