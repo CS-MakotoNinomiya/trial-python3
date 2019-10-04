aede33faf5d556733d77423ca4805b71f1269213
Ninomiya <m.ninomiya@central-soft.co.jp>
2019-10-04 10:44:58 +0900

    ファイル入出力部分をメンテナンス

--- trial001/trial001.py
+++ trial001/trial001.py
@@ -2,14 +2,16 @@ import os
 import datetime
 import pathlib
 
-base = os.path.dirname(os.path.abspath(__file__))
-name = os.path.normpath(os.path.join(base, "./conf/sample.txt"))
-
-print(pathlib.Path(name).exists())
+# 相対パスでファイルアクセス
+base_path = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.normpath(os.path.join(base_path, "./conf/sample.txt"))
+# ファイルの存在確認
+print(pathlib.Path(file_path).exists())
 
 try:
     dt_now = datetime.datetime.now()
-    file = open(name, 'a')
+    # 追記モードでファイルオープン
+    file = open(file_path, 'a')
     file.write("{0:%Y-%m-%d %H:%M:%S}".format(dt_now) + " write to file.\n")
 except Exception as e:
     print(e)
--- trial002/trial002.py
+++ trial002/trial002.py
@@ -1,24 +1,36 @@
 import os
+import datetime
 import pathlib
 
-base = os.path.dirname(os.path.abspath(__file__))
-name = os.path.normpath(os.path.join(base, "./conf/sample.txt"))
+# 相対パスでファイルパス
+path_base = os.path.dirname(os.path.abspath(__file__))
+path_file = os.path.normpath(os.path.join(path_base, "./tmp/sample.txt"))
 
-file_path = pathlib.Path(name)
 
-if file_path.exists():
+def datetime_now():
+    return datetime.datetime.now()
 
-    with file_path.open(mode='r') as f:
+
+with pathlib.Path(path_file).open(mode='a') as f:
+    print("ファイルに書き出し")
+    # 追記モードでファイルオープン
+    f.write("{0:%Y-%m-%d %H:%M:%S}".format(datetime_now()) + " start.\n")
+    f.write("{0:%Y-%m-%d %H:%M:%S}".format(datetime_now()) + " end.\n\n")
+    f.flush()
+
+with pathlib.Path(path_file).open(mode='r') as f:
+    print("ファイル読み込み（全行版）")
+    # 全行読み込み（行数が少ないことが分かっている場合）
+    lines = f.readlines()
+    for line in lines:
+        line = line.strip()    # remove line separator
+        print(line)
+
+with pathlib.Path(path_file).open(mode='r') as f:
+    print("ファイル読み込み（１行ずつ版）")
+    # 1行ずつ読み込み（行数が多い、多くなることが分かっている場合）
+    line = f.readline()
+    while line:
+        line = line.strip()    # remove line separator
+        print(line)
         line = f.readline()
-        while line:
-            line = line.rstrip('\r\n').rstrip('\n')    # remove line separator
-            print(line)
-            line = f.readline()
-        f.close()
-
-    with file_path.open(mode='r') as f:
-        lines = f.readlines()
-        f.close()
-        for line in lines:
-            line = line.rstrip('\r\n').rstrip('\n')    # remove line separator
-            print(line)
