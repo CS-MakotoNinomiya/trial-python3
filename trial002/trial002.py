f973ede16a15ce8854008e3453a58dec1e21664c
Ninomiya <m.ninomiya@central-soft.co.jp>
2019-10-04 17:26:41 +0900

    try-except の勉強

--- trial002/trial002.py
+++ trial002/trial002.py
@@ -8,29 +8,47 @@ path_file = os.path.normpath(os.path.join(path_base, "./tmp/sample.txt"))
 
 
 def datetime_now():
+    """ 現在日時を取得する.
+    \n説明文１
+    \n説明文２
+    """
     return datetime.datetime.now()
 
 
-with pathlib.Path(path_file).open(mode='a') as f:
-    print("ファイルに書き出し")
-    # 追記モードでファイルオープン
-    f.write("{0:%Y-%m-%d %H:%M:%S}".format(datetime_now()) + " start.\n")
-    f.write("{0:%Y-%m-%d %H:%M:%S}".format(datetime_now()) + " end.\n\n")
-    f.flush()
+try:
+    with pathlib.Path(path_file).open(mode='a') as f:
+        print("ファイルに書き出し")
+        # 追記モードでファイルオープン
+        f.write("{0:%Y-%m-%d %H:%M:%S}".format(datetime_now()) + " start.\n")
+        f.write("{0:%Y-%m-%d %H:%M:%S}".format(datetime_now()) + " end.\n\n")
+        f.flush()
+except Exception as e:
+    print(e)
 
-with pathlib.Path(path_file).open(mode='r') as f:
-    print("ファイル読み込み（全行版）")
-    # 全行読み込み（行数が少ないことが分かっている場合）
-    lines = f.readlines()
-    for line in lines:
-        line = line.strip()    # remove line separator
-        print(line)
+try:
+    with pathlib.Path(path_file).open(mode='r') as f:
+        print("ファイル読み込み（全行版）")
+        # 全行読み込み（行数が少ないことが分かっている場合）
+        lines = f.readlines()
+        line_array = []
+        for line in lines:
+            line = line.strip()    # remove line separator
+            line_array.append(line)
+            print(line)
+        print("2019-10-04 10:13:36 start." in line_array)
+except Exception as e:
+    print(e)
 
-with pathlib.Path(path_file).open(mode='r') as f:
-    print("ファイル読み込み（１行ずつ版）")
-    # 1行ずつ読み込み（行数が多い、多くなることが分かっている場合）
-    line = f.readline()
-    while line:
-        line = line.strip()    # remove line separator
-        print(line)
+try:
+    with pathlib.Path(path_file).open(mode='r') as f:
+        print("ファイル読み込み（１行ずつ版）")
+        # 1行ずつ読み込み（行数が多い、多くなることが分かっている場合）
         line = f.readline()
+        while line:
+            line = line.strip()    # remove line separator
+            if "2019-10-04 10:13:36 start." == line:
+                print("True")
+            print(line)
+            line = f.readline()
+except Exception as e:
+    print(e)
